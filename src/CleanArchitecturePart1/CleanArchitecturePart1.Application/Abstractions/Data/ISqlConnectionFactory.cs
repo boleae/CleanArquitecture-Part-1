947a184ef267b34cb1b44ca108f8bbce0b2823ec
ebolea <emilio.bolea@aranzadilaley.es>
2024-03-30 17:24:11 +0100

    Consulta en Dapper

---
+++ src/CleanArchitecturePart1/CleanArchitecturePart1.Application/Abstractions/Data/ISqlConnectionFactory.cs
@@ -0,0 +1,8 @@
+using System.Data;
+
+namespace ClearArchitecture.Application.Abstractions.Data;
+
+public interface ISqlConnectionFactory 
+{
+    IDbConnection CreateConnection();
+}
---
+++ src/CleanArchitecturePart1/CleanArchitecturePart1.Application/Alquileres/GetAlquiler/GetAlquilerQuery.cs
@@ -0,0 +1,5 @@
+using ClearArchitecture.Application.Abstractions.Messaging;
+
+namespace ClearArchitecture.Application.Alquileres.GetAlquiler;
+
+public sealed record GetAlquilerQuery(Guid AlquilerId) : IQuery<AlquilerResponse>;
