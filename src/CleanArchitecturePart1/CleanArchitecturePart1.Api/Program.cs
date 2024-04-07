45902cfcce043fefc97ed1698bb97681f2490c6d
ebolea <emilio.bolea@aranzadilaley.es>
2024-04-07 18:17:21 +0200

    Creacion de Controllers y Setup de postgre Database

--- src/CleanArchitecturePart1/CleanArchitecturePart1.Api/Program.cs
+++ src/CleanArchitecturePart1/CleanArchitecturePart1.Api/Program.cs
@@ -1,3 +1,6 @@
+using CleanArchitecture.Application;
+using CleanArchitecture.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
@@ -5,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
