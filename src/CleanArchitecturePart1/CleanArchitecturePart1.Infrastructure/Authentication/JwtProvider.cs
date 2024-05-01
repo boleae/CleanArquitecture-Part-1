f0ccab9f1586afa23b3d5c429796d02dc39a1ff6
ebolea <emilio.bolea@aranzadilaley.es>
2024-05-01 22:16:23 +0200

    Generacion de Jwt con Permissions

--- src/CleanArchitecturePart1/CleanArchitecturePart1.Infrastructure/Authentication/JwtProvider.cs
+++ src/CleanArchitecturePart1/CleanArchitecturePart1.Infrastructure/Authentication/JwtProvider.cs
@@ -2,7 +2,9 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using CleanArchitecture.Application.Abstractions.Authentication;
+using CleanArchitecture.Application.Abstractions.Data;
 using CleanArchitecture.Domain.Users;
+using Dapper;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
@@ -11,19 +13,46 @@ namespace CleanArchitecture.Infrastructure.Authentication;
 public sealed class JwtProvider : IJwtProvider
 {
     private readonly JwtOptions _options;
+    private readonly ISqlConnectionFactory _sqlConnectionFactory;
 
-    public JwtProvider(IOptions<JwtOptions> options)
+    public JwtProvider(IOptions<JwtOptions> options, ISqlConnectionFactory sqlConnectionFactory)
     {
         _options = options.Value;
+        _sqlConnectionFactory = sqlConnectionFactory;
     }
 
-    public Task<string> Generate(User user)
+    public async Task<string> Generate(User user)
     {
+
+        const string sql = """
+            SELECT 
+                p.nombre
+            FROM users usr
+                LEFT JOIN users_roles usrl
+                    ON usr.id = usrl.user_id
+                LEFT JOIN roles rl
+                    ON rl.id = usrl.role_id
+                LEFT JOIN roles_permissions rp
+                    ON rl.id = rp.role_id
+                LEFT JOINS permissions p
+                    ON p.id = rp.permission_id
+            WHERE usr.id = @UserId
+        """;
+
+        using var connection = _sqlConnectionFactory.CreateConnection();
+        var permissions = await connection.QueryAsync(sql, new { UserId = user.Id!.Value});
+        var permissionsCollection = permissions.ToHashSet();
+        
         var claims = new List<Claim> {
             new Claim(JwtRegisteredClaimNames.Sub, user.Id!.ToString()),
             new Claim(JwtRegisteredClaimNames.Email, user.Email!.ToString())
 
         };
+
+        foreach(var permission in permissionsCollection)
+        {
+            claims.Add(new (CustomClaims.Permissions, permission));
+        }
 
         var signinCredentials = new SigningCredentials( 
             new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey!)),
@@ -40,6 +69,6 @@ public sealed class JwtProvider : IJwtProvider
                                             
                                             );
         var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
-        return Task.FromResult<string>(tokenValue);
+        return tokenValue;
     }
 }
