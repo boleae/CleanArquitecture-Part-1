466d09aeab81a896f896211ff26e72105c9086a9
ebolea <emilio.bolea@aranzadilaley.es>
2024-05-02 09:50:21 +0200

    Controller y Authorization avanzado

--- src/CleanArchitecturePart1/CleanArchitecturePart1.Infrastructure/Authentication/JwtProvider.cs
+++ src/CleanArchitecturePart1/CleanArchitecturePart1.Infrastructure/Authentication/JwtProvider.cs
@@ -12,10 +12,13 @@ namespace CleanArchitecture.Infrastructure.Authentication;
 
 public sealed class JwtProvider : IJwtProvider
 {
-    private readonly JwtOptions _options;
+   private readonly JwtOptions _options;
     private readonly ISqlConnectionFactory _sqlConnectionFactory;
 
-    public JwtProvider(IOptions<JwtOptions> options, ISqlConnectionFactory sqlConnectionFactory)
+    public JwtProvider(
+        IOptions<JwtOptions> options,
+        ISqlConnectionFactory sqlConnectionFactory
+        )
     {
         _options = options.Value;
         _sqlConnectionFactory = sqlConnectionFactory;
@@ -23,52 +26,56 @@ public sealed class JwtProvider : IJwtProvider
 
     public async Task<string> Generate(User user)
     {
-
         const string sql = """
-            SELECT 
+              SELECT 
                 p.nombre
-            FROM users usr
+              FROM users usr
                 LEFT JOIN users_roles usrl
-                    ON usr.id = usrl.user_id
+                    ON usr.id=usrl.user_id
                 LEFT JOIN roles rl
-                    ON rl.id = usrl.role_id
+                    ON rl.id=usrl.role_id
                 LEFT JOIN roles_permissions rp
-                    ON rl.id = rp.role_id
-                LEFT JOINS permissions p
-                    ON p.id = rp.permission_id
-            WHERE usr.id = @UserId
+                    ON rl.id=rp.role_id
+                LEFT JOIN permissions p
+                    ON p.id=rp.permission_id
+                WHERE usr.id=@UserId            
         """;
 
         using var connection = _sqlConnectionFactory.CreateConnection();
-        var permissions = await connection.QueryAsync(sql, new { UserId = user.Id!.Value});
-        var permissionsCollection = permissions.ToHashSet();
+        var permissions = 
+        await connection.QueryAsync<string>(sql, new { UserId = user.Id!.Value });
+        
+        var permissionCollection = permissions.ToHashSet();
         
         var claims = new List<Claim> {
             new Claim(JwtRegisteredClaimNames.Sub, user.Id!.ToString()),
-            new Claim(JwtRegisteredClaimNames.Email, user.Email!.ToString())
-
+            new Claim(JwtRegisteredClaimNames.Email, user.Email!.Value)
         };
 
-        foreach(var permission in permissionsCollection)
+        foreach(var permission in permissionCollection)
         {
             claims.Add(new (CustomClaims.Permissions, permission));
         }
 
-        var signinCredentials = new SigningCredentials( 
+
+        var sigingCredentials = new SigningCredentials(
             new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey!)),
             SecurityAlgorithms.HmacSha256
-
         );
 
-        var token = new JwtSecurityToken(_options.Issuer, 
-                                            _options.Audience, 
-                                            claims,
-                                            null,
-                                            DateTime.UtcNow.AddDays(365),
-                                            signinCredentials
-                                            
-                                            );
+        var token = new JwtSecurityToken(
+            _options.Issuer,
+            _options.Audience,
+            claims,
+            null,
+            DateTime.UtcNow.AddDays(365),
+            sigingCredentials
+        );
+
         var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+        
         return tokenValue;
+
     }
 }
